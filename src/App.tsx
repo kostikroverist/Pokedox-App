36dfc639e1ec5f8f698a7a0382250fcfffbf5e8f
kostikroverist <kostikzhora@gmail.com>
2024-01-24 07:23:29 +0200

    create project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="w-full h-screen flex flex-cols text-center">
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Simple React Typescript Tailwind Sample
+      </h1>
+    </div >
+  )
+}
+
+export default App
